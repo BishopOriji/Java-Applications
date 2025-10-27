16e969d889cd03bdbb278602f3b842f4a124b296
BishopOriji <trendybishop@yahoo.com>
2025-10-27 14:43:19 +0100

    First Java Programs

---
+++ TypeCasting.java
@@ -0,0 +1,9 @@
+public class TypeCasting {
+    public static void main(String[] var0) {
+        double var1 = (double)10.0F;
+        System.out.printf("The value is %f%n", var1);
+        double var3 = 32.89;
+        int var5 = (int)var3;
+        System.out.printf("The value is %d%n", var5);
+    }
+}
